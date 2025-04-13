fa50a98c89d15321919b5308abe96df70a29d5d3
Tripp Shelnutt <trippshelnutt@outlook.com>
2025-04-13 08:57:29 -0400

    test api method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Typography, Container } from '@mui/material';
 
-export default function Home() {
+async function getTitle() {
+  const res = await fetch('http://localhost:3000/api/title', { cache: 'no-store' });
+  const data = await res.json();
+  return data.title;
+}
+
+export default async function Home() {
+  const title = await getTitle();
+  
   return (
     <Container>
       <Typography variant="h2" component="h1" sx={{ textAlign: 'center', mt: 4 }}>
-        Transaction Sorter
+        {title}
       </Typography>
     </Container>
   );
